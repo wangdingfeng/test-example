22eb1ac386404633ee7ca3f5694024e5e1716439
wangdingfeng@live.com <feng5211314>
2018-04-22 22:24:29 +0800

    流程类

--- src/main/java/com/example/testexample/TestController.java
+++ src/main/java/com/example/testexample/TestController.java
@@ -1,8 +1,16 @@
 package com.example.testexample;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.process.TaskQueryRequest;
+import com.example.process.TaskQueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author wangdingfeng
@@ -13,9 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping(value = "/test",method = RequestMethod.GET)
-    public String test(){
+    public Object test(){
         System.out.println("234234");
-        return "123123123123";
+
+//        HttpEntity<TaskQueryRequest> taskSubmitRequestEntity = new HttpEntity<>(new TaskQueryRequest());
+
+//        ResponseEntity<TaskQueryResponse> responseEntity = restTemplate.exchange(
+//                "http://39.107.110.75:8088//activiti-rest/service/repository/process-definitions",
+//                HttpMethod.POST,
+//                taskSubmitRequestEntity,
+//                TaskQueryResponse.class
+//        );
+        //获取所有任务
+        JSONObject json = restTemplate.getForEntity("http://39.107.110.75:8088//activiti-rest/service/repository/process-definitions", JSONObject.class).getBody();
+
+
+        return json;
     }
 }
